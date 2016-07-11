39103debf1a786fb794cdf1125f616515be23f1f
Metallidog <metallidogmooc@yahoo.com>
2016-07-11 11:39:56 -0700

    'first push, half done'

--- calculator/main.py
+++ calculator/main.py
@@ -4,7 +4,7 @@ from calculator.operations import *
 from calculator.exceptions import *
 
 
-def create_new_calculator(operations=None):
+def create_new_calculator(operations={}):
     """
     Creates a configuration dict for a new calculator. Optionally pre loads an
     initial set of operations. By default a calculator with no operations
@@ -13,7 +13,10 @@ def create_new_calculator(operations=None):
     :param operations: Dict with initial operations.
                        ie: {'sum': sum_function, ...}
     """
-    pass
+    return {
+            'operations': operations,
+            'history': []
+    }
 
 
 def perform_operation(calc, operation, params):
@@ -26,8 +29,8 @@ def perform_operation(calc, operation, params):
     :param params: Tuple containing the list of nums to operate with.
                    ie: (1, 2, 3, 4.5, -2)
     """
-    pass
-
+    calc['history'].append(('timeofday', operation, params))
+    return calc['operations'][operation](*params)
 
 def add_new_operation(calc, operation):
     """
@@ -37,14 +40,15 @@ def add_new_operation(calc, operation):
     :param operation: Dict with the single operation to be added.
                       ie: {'add': add_function}
     """
-    pass
+    calc['operations'].update(operation)
+        
 
 
 def get_operations(calc):
     """
     Returns the list of operation names supported by given calculator.
     """
-    pass
+    return calc['operations'].keys()
 
 
 def get_history(calc):
@@ -58,18 +62,21 @@ def get_history(calc):
         ie:
         ('2016-05-20 12:00:00', 'add', (1, 2), 3),
     """
-    pass
+    return calc['history']
 
 
 def reset_history(calc):
     """
     Resets the calculator history back to an empty list.
     """
-    pass
+    calc['history'] = []
 
 
 def repeat_last_operation(calc):
     """
     Returns the result of the last operation executed in the history.
     """
-    pass
+    if calc['history']:
+        return calc['operations'][calc['history'][-1][1]](*calc['history'][-1][2])
+    else:
+        None
--- calculator/operations.py
+++ calculator/operations.py
@@ -1,18 +1,31 @@
+from functools import reduce
 
 def add(*args):
-    # your implementation here
-    pass
+    if validate(args):
+        return sum(args)
+    else:
+        raise InvalidParams
 
 def subtract(*args):
-    # your implementation here
-    pass
+    return args[0] + -1*add(*args[1:])
 
 def multiply(*args):
-    # your implementation here
-    pass
+    if args:
+        return reduce(lambda a,b: a*b, args) 
+    else:
+        return 0
 
 def divide(*args):
-    # your implementation here
-    pass
+    if args:
+        if len(args)>1:
+            return float(args[0]) / multiply(*args[1:])
+        else:
+            return args[0]
+    else:
+        return 0
 
-# add your custom operations here
+def validate(args):
+    if any(isinstance(a, str) for a in args):
+        return False
+    else:
+        return True
